d1c5d41f94a5aaa57cf9da9e13e0eac250f28896
Ali-Sina-255 <alisinasultani255@gmail.com>
2024-04-02 15:57:16 +0430

    fetch data in to home page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blogcatagory.models import Category, Blog
 
 
 def home(request):
-    return render(request, 'blog/home-blogs.html')
+    categorys = Category.objects.all()
+    blog_featuers = Blog.objects.filter(is_featured=True, status="Published" )
+    posts = Blog.objects.filter(is_featured=False, status="Published" )
+    context = {
+        'categorys': categorys,
+        "blog_featuers":blog_featuers,
+        "posts": posts
+    }
+    return render(request, 'blog/home-blogs.html', context)
